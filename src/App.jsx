d14649b5f2c41dc88cf365e9c5f24d2ef7a7990e
DeveloperYamin <developer.yamin@gmail.com>
2022-02-25 02:47:47 +0600

    create layout for this app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
+import { CssBaseline, Grid } from "@material-ui/core"
+import Header from "./components/Header";
+import List from "./components/List";
+import Map from "./components/Map";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
