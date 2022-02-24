bb287dcd6ef9f7a6b2a21743bc7728fd14979d66
DeveloperYamin <developer.yamin@gmail.com>
2022-02-25 03:09:58 +0600

    creating header component and styled

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { AppBar, Box, InputBase, Toolbar, Typography } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
 function Header() {
+  const classes = useStyles();
   return (
-    <div>Header</div>
-  )
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.title} variant="h5">
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+        </Box>
+        {/* <Autocomplete> */}
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{ root: classes.inputRoot, input: classes.inputInput }}
+          />
+        </div>
+        {/* <Autocomplete/> */}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
