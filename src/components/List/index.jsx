e03558ae805717b6ec7735415655a64f3f0fd734
DeveloperYamin <developer.yamin@gmail.com>
2022-02-25 12:25:03 +0600

    list file created

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@material-ui/core";
+import { useState } from "react";
+import PlaceDetails from "../PlaceDetails";
+import useStyles from "./styles";
 
 function List() {
-  return <div>List</div>;
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const places = [
+    { name: "cool" },
+    { name: "hot" },
+    { name: "normal" },
+    { name: "cool" },
+    { name: "hot" },
+    { name: "normal" },
+  ];
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Food & Dining around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="type">Type</InputLabel>
+        <Select
+          id="type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="rating">Rating</InputLabel>
+        <Select
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        >
+          <MenuItem value="">All</MenuItem>
+          <MenuItem value="3">Above 3.0</MenuItem>
+          <MenuItem value="4">Above 4.0</MenuItem>
+          <MenuItem value="4.5">Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid key={i} item xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
 
 export default List;
--- src/components/PlaceDetails/index.jsx
+++ src/components/PlaceDetails/index.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function PlaceDetails() {
   return <div>Place Details</div>;
 }
