d45ce36b7f4a48590d344bcf5e3b38ea1e41ac03
DeveloperYamin <developer.yamin@gmail.com>
2022-02-25 10:44:32 +0600

    add google map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useMediaQuery } from "@material-ui/core";
+import GoogleMapReact from "google-map-react";
+import useStyles from "./styles";
 
 function Map() {
-  return <div>Map</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+  const coordinates = {lat:0,lng:0};
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 }
 
 export default Map;
